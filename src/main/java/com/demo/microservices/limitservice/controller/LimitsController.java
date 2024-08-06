e62dfde0e7e50ab4fadc74430ad982dfcbf841d5
ashfaq-m <ashapak.makandar7@gmail.com>
2024-08-06 14:41:34 +0530

    Gets the limits from properties file

--- src/main/java/com/demo/microservices/limitservice/controller/LimitsController.java
+++ src/main/java/com/demo/microservices/limitservice/controller/LimitsController.java
@@ -1,6 +1,8 @@
 package com.demo.microservices.limitservice.controller;
 
+import com.demo.microservices.limitservice.config.Configuration;
 import com.demo.microservices.limitservice.entity.Limits;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("limits")
     public Limits getLimits(){
-        return new Limits(2, 88);
+        return new Limits(
+                configuration.getMinimum(),
+                configuration.getMaximum()
+        );
     }
 }
